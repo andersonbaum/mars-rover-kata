29654f9789975ccfe167e0ea4d42e59953ca44c7
Anderson Baum Barbosa <anderson.baum@gmail.com>
2018-06-30 13:29:47 -0300

    Creating the project structure with Springboot

---
+++ src/main/java/br/com/uniritter/tasima/arqweb/marsroverkata/MarsroverkataApplication.java
@@ -0,0 +1,12 @@
+package br.com.uniritter.tasima.arqweb.marsroverkata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarsroverkataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarsroverkataApplication.class, args);
+	}
+}
